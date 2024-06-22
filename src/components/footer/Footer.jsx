223122618782601f72ea9b3770b9228c96d84299
Hudson Souza <hudson.sza@gmail.com>
2024-06-22 02:52:11 -0400

    update dev url

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,6 @@
 import './Footer.css'
 
-export default function Footer() {http://localhost:5173/
+export default function Footer() {
     return (
       <div className="footer-container">
             <p>KDORE INTEGRAÇÃO E DESENVOLVIMENTO SUSTENTÁVEL © 2024</p>
--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://eventos-app-dev.kdore.com.br/api'
 });
 
 export default api;
